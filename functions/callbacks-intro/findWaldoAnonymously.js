627ccdf7b1090eb86802891197678663502c01be
daejung90 <djung.ca@gmail.com>
2022-11-18 10:14:54 -0500

    Anonymous Function exercise

---
+++ functions/callbacks-intro/findWaldoAnonymously.js
@@ -0,0 +1,16 @@
+const findWaldo = function(names, found) {
+    names.forEach((name, index) => {
+      if (name === 'Waldo') {
+        found(index);
+      }
+    });
+  };
+  
+    
+//   const actionWhenFound = function(index) {
+//     console.log("Found Waldo at index " + index + "!");
+//   };
+    
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index){
+    console.log("Found Waldo at index " + index + "!");
+  });
